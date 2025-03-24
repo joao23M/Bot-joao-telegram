29f2f61d9dc27a68227c80927a7d738b4aa6c3c2
joao23M <karikwia@gmail.com>
2025-03-23 21:49:26 -0300

    Corrigir token do bot
    Corrigido o token do bot para garantir que o deploy funcione corretamente.

--- bot.py
+++ bot.py
@@ -5,12 +5,15 @@ def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Bem-vindo ao JOAZINHO STORE!')
 
 def main() -> None:
+    # Token do seu bot (substitua pelo token correto)
     updater = Updater("7954905234:AAGibTxa20RT_rv2kl_fC4Yk9yq8kadgW4o")
 
     dispatcher = updater.dispatcher
 
+    # Adiciona o comando /start
     dispatcher.add_handler(CommandHandler("start", start))
 
+    # Inicia o bot
     updater.start_polling()
     updater.idle()
 
